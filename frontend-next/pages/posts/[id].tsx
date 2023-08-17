355969efe85ee48f83816fb0dc70e45a0f6c14ac
tran-doan <trandoan92ag@gmail.com>
2023-08-17 16:51:15 +0700

    Add publish/unpublish/delete post features

--- frontend-next/pages/posts/[id].tsx
+++ frontend-next/pages/posts/[id].tsx
@@ -4,6 +4,8 @@ import ReactMarkdown from 'react-markdown';
 import Layout from '../../components/Layout';
 import { PostProps } from '../../components/Post';
 import prisma from '../../lib/prisma';
+import Router from 'next/router';
+import { useSession } from 'next-auth/react';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findFirst({
@@ -12,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   });
@@ -21,7 +23,34 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 };
 
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/post/publish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+}
+
+async function unpublishPost(id: string): Promise<void> {
+  await fetch(`/api/post/unpublish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+}
+
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/delete/${id}`, {
+    method: 'DELETE',
+  });
+  await Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
+  const { data: session, status } = useSession();
+  if (status === 'loading') {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
   let title = props.title;
   if (!props.published) {
     title = `${title} (Draft)`;
@@ -33,10 +62,15 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && <button onClick={() => unpublishPost(props.id)}>Unpublish</button>}
+        {userHasValidSession && postBelongsToUser && <button onClick={() => deletePost(props.id)}>Delete</button>}
       </div>
       <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
