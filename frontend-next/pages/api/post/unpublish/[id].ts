2a181aac86accf7fbe47c0bd0f87118d9e20f4eb
tran-doan <trandoan92ag@gmail.com>
2023-08-24 10:40:04 +0700

    Correct unpublish post implementattion; correct redirect url to /drafts after creating post

--- frontend-next/pages/api/post/unpublish/[id].ts
+++ frontend-next/pages/api/post/unpublish/[id].ts
@@ -1,14 +1,11 @@
 import prisma from '../../../../lib/prisma';
 
-// PUT /api/publish/:id
+// PUT /api/unpublish/:id
 export default async function handle(req, res) {
   const postId = req.query.id;
-  if (req.method === 'DELETE') {
-    const post = await prisma.post.delete({
-      where: { id: postId },
-    });
-    res.json(post);
-  } else {
-    throw new Error(`The HTTP ${req.method} method is not supported at this route.`);
-  }
+  const post = await prisma.post.update({
+    where: { id: postId },
+    data: { published: false },
+  });
+  res.json(post);
 }
