c26ccf7fddf73c4e3a279b32152d6fed8acb5817
tran-doan <trandoan92ag@gmail.com>
2023-09-28 09:46:49 +0700

    Solve CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ const whitelist = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://10.0.2.2:3000',
-  'https://tran-zuhlke.github.io/',
+  'https://tran-zuhlke.github.io',
 ];
 
 const bootstrap = async () => {
